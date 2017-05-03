3b6422cbb0da98604ff36394a4dfce8ba4909ed5
Elena <lena1687@mail.ru>
2017-05-03 10:58:45 +0600

    Version project from 03.05.2017

---
+++ copy pages/front-page.php
@@ -0,0 +1,50 @@
+<div class="galary">
+<div class="main-heading">
+<div class="wave">
+</div>
+<h2 class="heading">Galary</h2>
+</div>
+<div class="clearfix mosaicflow" data-min-item-width="250">
+<div class="mosaicflow__item"><a class="fancybox" title="1" href="/wp-content/uploads/2017/04/galary-1.png" data-fancybox="group" data-type="image" data-caption="Caption #1">
+<img src="/wp-content/uploads/2017/04/galary-1.png" alt="1" height="235px" />
+</a></div>
+<div class="mosaicflow__item"><a class="fancybox" title="2" href="/wp-content/uploads/2017/04/galary-2.png" data-fancybox="group" data-type="image" data-caption="Caption #2">
+<img src="/wp-content/uploads/2017/04/galary-2.png" alt="2" height="174px" />
+</a></div>
+<div class="mosaicflow__item"><a class="fancybox" title="3" href="/wp-content/uploads/2017/04/galary-3.png" data-fancybox="group" data-type="image" data-caption="Caption #3">
+<img src="/wp-content/uploads/2017/04/galary-3.png" alt="3" height="232px" />
+</a></div>
+<div class="mosaicflow__item"><a class="fancybox" title="4" href="/wp-content/uploads/2017/04/galary-4.png" data-fancybox="group" data-type="image" data-caption="Caption #4">
+<img src="/wp-content/uploads/2017/04/galary-4.png" alt="4" height="200px" />
+</a></div>
+<div class="mosaicflow__item"><a class="fancybox" title="5" href="/wp-content/uploads/2017/04/galary-5.png" data-fancybox="group" data-type="image" data-caption="Caption #5">
+<img src="/wp-content/uploads/2017/04/galary-5.png" alt="5" height="260px" />
+</a></div>
+<div class="mosaicflow__item"><a class="fancybox" title="6" href="/wp-content/uploads/2017/04/galary-6.png" data-fancybox="group" data-type="image" data-caption="Caption #6">
+<img src="/wp-content/uploads/2017/04/galary-6.png" alt="6" height="200px" />
+</a></div>
+</div>
+</div>
+</section>
+<div class="galary-bottom">
+<div class="container">
+<div class="logos"><a href="https://slack.com" target="blank">
+<img src="/wp-content/uploads/2017/04/galary-bottom-1.png" alt="logo" />
+</a></div>
+<div class="logos"><a href="www.adobe.com/ru" target="blank">
+<img src="/wp-content/uploads/2017/04/galary-bottom-2.png" alt="logo" />
+</a></div>
+<div class="logos"><a href="https://www.google.ru" target="blank">
+<img src="/wp-content/uploads/2017/04/galary-bottom-3.png" alt="logo" />
+</a></div>
+<div class="logos"><a href="https://facebook.com" target="blank">
+<img src="/wp-content/uploads/2017/04/galary-bottom-4.png" alt="logo" />
+</a></div>
+<div class="logos"><a href="https://www.spotify.com" target="blank">
+<img src="/wp-content/uploads/2017/04/galary-bottom-5.png" alt="logo" />
+</a></div>
+<div class="logos"><a href="http://www.nike.com/ru/ru_ru/" target="blank">
+<img src="/wp-content/uploads/2017/04/galary-bottom-6.png" alt="logo" />
+</a></div>
+</div>
+</div>
